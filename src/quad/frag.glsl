7ed096bf134ded1006c2c8164995323336c08218
arilotter <arilotter@gmail.com>
2017-06-20 14:27:27 -0400

    trippy stuff

--- src/quad/frag.glsl
+++ src/quad/frag.glsl
@@ -8,9 +8,11 @@ varying vec2 vUv;
 void main() {
   // UV coords are inverted
   vec2 uv = 1.0 - vUv;
-  float n = noise(vec3(uv * 1.2, time / 30.0));
-  float x = mod(uv.x + n, 1.0) * time;
-  float y = mod(uv.y + n, 1.0) * time;
-  gl_FragColor = mix(texture2D(texture, vec2(sin(x), y)), vec4(n, x, y, 1.0), n);
+  float n = noise(vec3(uv * 1.2, time / 30.0)) * 0.5;
+  n += noise(vec3(uv * 50., time / 15.0)) * 0.01;
+
+  float x = mod(uv.x + n, 1.0);
+  float y = mod(uv.y + n, 1.0);
+  gl_FragColor = texture2D(texture, vec2(y, x));
   // gl_FragColor = vec4(uv, 0.0, 1.0);
 }
