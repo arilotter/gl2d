4c3b077ff5e03bf8781ac6f3cbbad9b75c692a06
arilotter <arilotter@gmail.com>
2017-06-20 14:16:20 -0400

    trippy colors

--- src/quad/frag.glsl
+++ src/quad/frag.glsl
@@ -3,14 +3,14 @@ uniform sampler2D texture;
 uniform float time;
 varying vec2 vUv;
 
+#pragma glslify: noise = require(glsl-noise/simplex/3d)
+
 void main() {
   // UV coords are inverted
   vec2 uv = 1.0 - vUv;
-  float pi = asin(-1.0);
-  float t = 0.0;
-  float s = sin(t), c = cos(t);
-  uv += 0.3 * sin(uv.yx * 8.0 + s) - vec2(c, s) * 0.1; // wavy effect
-  gl_FragColor = texture2D(texture, vec2(uv.x, uv.y));
-  // gl_FragColor = texture2D(texture, uv) + vec4(uv * sin(time), 0.0, 1.0);
+  float n = noise(vec3(uv * 1.2, time / 30.0));
+  float x = mod(uv.x + n, 1.0) * time;
+  float y = mod(uv.y + n, 1.0) * time;
+  gl_FragColor = mix(texture2D(texture, vec2(sin(x), y)), vec4(n, x, y, 1.0), n);
   // gl_FragColor = vec4(uv, 0.0, 1.0);
 }
