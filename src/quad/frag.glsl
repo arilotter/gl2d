51ff6446d6bc988f343da29e3551bf0f1db1db57
arilotter <arilotter@gmail.com>
2017-06-20 17:18:16 -0400

    crazy eyes

--- src/quad/frag.glsl
+++ src/quad/frag.glsl
@@ -5,19 +5,19 @@ varying vec2 vUv;
 
 #pragma glslify: noise = require(glsl-noise/simplex/2d)
 
-float noisify(float a) {
+float noisify(vec2 a) {
   float n = 0.;
-  n += noise(vec2(a * 1.2, a + time / 30.0)) * 0.5;
-  n += noise(vec2(sin(a) * 10., a + time / 15.0)) * 0.01;
+  n += noise(vec2(a.x * 10.2, a.y + time / 30.0)) * 0.5;
+  n += noise(vec2(sin(a.x) * 10., a.y + time / 15.0)) * 0.01;
   return n;
 }
 
 void main() {
   // UV coords are inverted
   vec2 uv = 1.0 - vUv;
-  float x = mod(uv.x + noisify(uv.y), 1.0);
-  float y = mod(uv.y + noisify(uv.x), 1.0);
-  // gl_FragColor = vec4(y, x, 0.0, 1.0);
+  float x = noisify(uv.xy) / 2. + 0.3;
+  float y = noisify(uv.yx) / 2. + 0.3; 
+  // gl_FragColor = vec4(x / 2. + 0.3, 0., 0.0, 1.0);
   gl_FragColor = texture2D(texture, vec2(y, x));
   // gl_FragColor = vec4(uv, 0.0, 1.0);
 }
